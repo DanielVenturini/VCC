27ca7a6834075107550a8d0a0bd0a1db72c4b14e
Daniel Venturini <danielventurini021@gmail.com>
2018-09-14 23:31:30 -0300

    Resolvido erro de sinal após número seguido de número

--- lexical/varredura.c
+++ lexical/varredura.c
@@ -431,7 +431,7 @@ TokenRecord* getToken(void){
                 token->numcaracter = numcaracters;
                 token->tokenval = (*c=='+') ? SOMA : SUBTRACAO;
 
-                if(ultimoToken != NUM_I && ultimoToken != NUM_F && ultimoToken != FECHA_PARENTESES && ultimoToken != FECHA_COLCHETES) {
+                if(ultimoToken != ID && ultimoToken != NUM_I && ultimoToken != NUM_F && ultimoToken != FECHA_PARENTESES && ultimoToken != FECHA_COLCHETES) {
 
                     getCaracter();                  /// se o próximo caracter for número, então este sinal pertence a este número
                     while(*c == ' '){               /// enquanto estiver lendo somente espaços
